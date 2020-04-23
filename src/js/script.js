0a77dcd9954dcdfa04cc8fa2401bd6d90e07c2bc
Bartosz Glinka <bartosz.glinka@hotmail.com--global>
2020-04-23 16:17:26 +0200

    correct fails npm test:js - 'eslint' in script.js

--- src/js/script.js
+++ src/js/script.js
@@ -85,10 +85,10 @@
     initAccordion(){
       const thisProduct = this;
       /* find the clickable trigger (the element that should react to clicking) */
-      const buttonOpen = thisProduct.element.querySelector(select.menuProduct.clickable)
-      
+      const buttonOpen = thisProduct.element.querySelector(select.menuProduct.clickable);
+
       /* START: click event listener to trigger */
-        buttonOpen.addEventListener('click', function(){
+      buttonOpen.addEventListener('click', function(){
           
         /* prevent default action for event */
         event.preventDefault();
@@ -97,21 +97,21 @@
         thisProduct.element.classList.toggle('active');
           
         /* find all active products */
-        const activeProducts = document.querySelectorAll(select.all.menuProductsActive)
-        console.log(activeProducts)
+        const activeProducts = document.querySelectorAll(select.all.menuProductsActive);
+        console.log(activeProducts);
         /* START LOOP: for each active product */
         for(let activeProduct of activeProducts){
           /* START: if the active product isn't the element of thisProduct */
           
           if(activeProduct != thisProduct.element){
             /* remove class active for the active product */
-              activeProduct.classList.remove('active');
+            activeProduct.classList.remove('active');
           /* END: if the active product isn't the element of thisProduct */
           }
         /* END LOOP: for each active product */
         }
       /* END: click event listener to trigger */
-        })
+      });
     }
   }
   const app = {
